0500e150acb45b8743edc458184fbb675aa5d24b
KirillIvano <ivannadez@yandex.ru>
2020-08-09 03:52:46 +0300

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,7 +15,7 @@ class Router {
 
     private isInitialized = false;
 
-    private links: [HTMLLinkElement, () => void][] = [];
+    private links: HTMLAnchorElement[] = [];
 
     private currentPath = null;
     private handleContentUnload: undefined | (() => void) = null;
@@ -59,35 +59,50 @@ class Router {
         );
     }
 
+    private handleLinkClick = (e: MouseEvent) => {
+        e.preventDefault();
+        this.navigate((e.currentTarget as HTMLAnchorElement).href);
+    };
+
+
+    private handleLinkEnterPress = (e: KeyboardEvent) => {
+        if (e.key !== 'Enter') return;
+
+        e.preventDefault();
+        this.navigate((e.currentTarget as HTMLAnchorElement).href);
+    }
+
+    private enhanceLink = (link: HTMLAnchorElement) => {
+        link.addEventListener('click', this.handleLinkClick);
+        link.addEventListener('keyup', this.handleLinkEnterPress);
+        link.tabIndex = 0;
+
+        return link;
+    }
+
     // create links for the rest of the document
     private createInitialLinks(): void {
         const commonLinks = Array.prototype.slice.call(
             document.getElementsByClassName('redirect-link'),
-        ) as HTMLLinkElement[];
+        ) as HTMLAnchorElement[];
 
-        commonLinks.forEach(
-            link => link.addEventListener('click', () => this.navigate(link.dataset.route)),
-        );
+        commonLinks.forEach(this.enhanceLink);
     }
     // create links for routed part
     private updateLinks(): void {
         const links = Array.prototype.slice.call(
             this.parent.getElementsByClassName('redirect-link'),
-        ) as HTMLLinkElement[];
+        ) as HTMLAnchorElement[];
 
-        this.links = links.map(
-            link => {
-                const clickHandler = () => this.navigate(link.dataset.route);
-
-                link.addEventListener('click', clickHandler);
-
-                return [link, clickHandler];
-            },
-        );
+        this.links = links.map(this.enhanceLink);
     }
+
     // remove links for routed part
     private removeLinks(): void {
-        this.links.forEach(([link, handler]) => link.removeEventListener('click', handler));
+        this.links.forEach(link => {
+            link.removeEventListener('click', this.handleLinkClick);
+            link.removeEventListener('keyup', this.handleLinkEnterPress);
+        });
 
         this.links = [];
     }
@@ -107,7 +122,7 @@ class Router {
 
     private async handlePageOpen(page: Page, params?: string[]) {
         if (page.handleContentLoad) {
-            this.handleContentUnload = await page.handleContentLoad(this.parent, params) || null;
+            this.handleContentUnload = (await page.handleContentLoad(this.parent, params)) || null;
         }
 
         page.parts && await this.executeParts(page.parts, params);
@@ -128,7 +143,7 @@ class Router {
         if (!this.isInitialized) throw new Error('Router is not initialized');
 
         this.currentPath = path;
-        this.handlePageClose && this.handlePageClose();
+        this.handlePageClose();
 
         const routeConfig = this.routes.find(
             route => route.path instanceof RegExp ?
@@ -154,8 +169,6 @@ class Router {
         if (this.currentPath === path) return;
 
         history.pushState(null, null, path);
-
-        await this.update(path);
     }
 }
 
